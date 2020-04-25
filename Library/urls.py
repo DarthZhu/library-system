558060eb36d470d6d602cce551a809c58592781b
DarthZhu <darthzhu@gmail.com>
2020-04-26 00:13:33 +0800

    Starton function in the system
    Finish  login and register
    Begin building functions for admins

--- Library/urls.py
+++ Library/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('index/', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('admin/index', views.admin_index, name='admin_index'),
+    path('admin/<int:id>/index', views.admin_index, name='admin_index'),
     path('user/index', views.user_index, name='user_index'),
+    path('admin/<int:id>/booklist', views.admin_booklist, name='admin_booklist'),
+    path('admin/<int:id>/search', views.admin_search, name='admin_search'),
 ]
--- Library/views.py
+++ Library/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from Library.models import User, Administrator
+from Library.models import User, Administrator, Book
 
 
 def index(request):
@@ -47,27 +47,75 @@ def login(request):
     else:
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user_exist = User.objects.filter(user_name=username).count()
+        # print(username)
+        # print(password)
+        user_exist = User.objects.filter(user_username=username).count()
         admin_exist = Administrator.objects.filter(admin_username=username).count()
-        if user_exist + admin_exist:
+        # print(user_exist)
+        # print(admin_exist)
+        if user_exist + admin_exist == 0:
             return HttpResponse('User not exist')
         elif user_exist:
-            user = User.objects.filter(user_name=username)
+            user = User.objects.filter(user_username=username)
+            user = list(user)
+            user = user[0]
             if password == user.user_password:
-                return redirect(reverse('library:user_index'))
+                return redirect(reverse('library:user_index', kwargs={'id': user.id}))
             else:
                 return HttpResponse('Invalid password or username')
         else:
-            administrator = Administrator.objects.filter(user_name=username)
+            administrator = Administrator.objects.filter(admin_username=username)
+            administrator = list(administrator)
+            administrator = administrator[0]
             if password == administrator.admin_password:
-                return redirect(reverse('library:admin_index'))
+                return redirect(reverse('library:admin_index', kwargs={'id': administrator.id}))
             else:
                 return HttpResponse('Invalid password or username')
 
 
-def user_index(request):
+def user_index(request, id):
     return HttpResponse('Welcome User!')
 
 
-def admin_index(request):
-    return HttpResponse('Welcome Admin!')
+def admin_index(request, id):
+    data = {
+        "id": id,
+    }
+    return render(request, 'admin_index.html', context=data)
+
+
+def admin_booklist(request, id):
+    books = Book.objects.all()
+    data = {
+        "books": books,
+        "id": id,
+    }
+    return render(request, 'booklist.html', context=data)
+
+
+def admin_search(request, id):
+    if request.method == "GET":
+        data = {
+            "title": "search",
+            "id": id,
+        }
+        return render(request, 'admin_search.html', context=data)
+    else:
+        book_name = request.POST.get('book_name')
+        isbn = request.POST.get('ISBN')
+        author = request.POST.get('author')
+        publisher = request.POST.get('publisher')
+        result = Book.objects.all()
+        if book_name:
+            result = result.filter(bookname=book_name)
+        if isbn:
+            result = result.filter(ISBN=isbn)
+        if author:
+            result = result.filter(author=author)
+        if publisher:
+            result = result.filter(publisher=publisher)
+        data = {
+            "books": result,
+            "id": id,
+        }
+        return render(request, 'admin_search.html', context=data)
